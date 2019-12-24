b7f2794ba9f674b81755ea12ed0952769bb0f6b6
Sudarshan Saxena <sudarshansaxena@Sudarshans-MacBook-Air.local>
2019-12-24 15:39:20 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import faker from "faker";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img src={faker.image.avatar()} />
         </a>
-      </header>
+        <div className='content'>
+          <a href='#' className='author'>
+            Sudarshan Saxena
+          </a>
+          <div class='metadata'>
+            <span class='date'>Today at 5:42PM</span>
+          </div>
+          <div class='text'>How artistic!</div>
+        </div>
+      </div>
+
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img src={faker.image.avatar()} />
+        </a>
+        <div className='content'>
+          <a href='#' className='author'>
+            Sudarshan Saxena
+          </a>
+          <div class='metadata'>
+            <span class='date'>Today at 5:42PM</span>
+          </div>
+          <div class='text'>How artistic!</div>
+        </div>
+      </div>
+
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img src={faker.image.avatar()} />
+        </a>
+        <div className='content'>
+          <a href='#' className='author'>
+            Sudarshan Saxena
+          </a>
+          <div class='metadata'>
+            <span class='date'>Today at 5:42PM</span>
+          </div>
+          <div class='text'>How artistic!</div>
+        </div>
+      </div>
     </div>
   );
 }
