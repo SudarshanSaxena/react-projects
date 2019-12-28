72db56d87d17be1d4e906cbdc01817cb553b2615
Sudarshan Saxena <sudarshansaxena@Sudarshans-MacBook-Air.local>
2019-12-28 15:44:35 +0530

    learning done for this section

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import faker from "faker";
+import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
 import "./App.css";
 
 function App() {
   return (
     <div className='ui container comments'>
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img src={faker.image.avatar()} />
-        </a>
-        <div className='content'>
-          <a href='#' className='author'>
-            Sudarshan Saxena
-          </a>
-          <div class='metadata'>
-            <span class='date'>Today at 5:42PM</span>
-          </div>
-          <div class='text'>How artistic!</div>
-        </div>
-      </div>
-
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img src={faker.image.avatar()} />
-        </a>
-        <div className='content'>
-          <a href='#' className='author'>
-            Sudarshan Saxena
-          </a>
-          <div class='metadata'>
-            <span class='date'>Today at 5:42PM</span>
-          </div>
-          <div class='text'>How artistic!</div>
-        </div>
-      </div>
-
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img src={faker.image.avatar()} />
-        </a>
-        <div className='content'>
-          <a href='#' className='author'>
-            Sudarshan Saxena
-          </a>
-          <div class='metadata'>
-            <span class='date'>Today at 5:42PM</span>
-          </div>
-          <div class='text'>How artistic!</div>
-        </div>
-      </div>
+      <ApprovalCard>
+        <CommentDetails
+          author={faker.name.firstName()}
+          timeAgo='Today at 5PM!'
+          textContent={faker.lorem.text()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
